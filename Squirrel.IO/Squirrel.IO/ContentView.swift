18fc2a40bf4c74024f0499dc3ff06cd05900ad3c
Michael Amaya <mikeamaya8@gmail.com>
2023-03-02 15:53:42 -0500

    Add project files to git

---
+++ Squirrel.IO/Squirrel.IO/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Squirrel.IO
+//
+//  Created by Michael Amaya on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Squirrel.IO/Squirrel.IO/Squirrel_IOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Squirrel_IOApp.swift
+//  Squirrel.IO
+//
+//  Created by Michael Amaya on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Squirrel_IOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
