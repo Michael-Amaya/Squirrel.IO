0b182e3ce46903e0897b897eee65e68784133f9c
Michael Amaya <mikeamaya8@gmail.com>
2023-03-09 20:04:19 -0500

    Add firebase + firebase auth to project

--- Squirrel.IO/Squirrel.IO/Squirrel_IOApp.swift
+++ Squirrel.IO/Squirrel.IO/Squirrel_IOApp.swift
@@ -9,9 +9,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct Squirrel_IOApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             LaunchScreen()      //launch Screen displays logo and name, then HomePage view after 2 secs
